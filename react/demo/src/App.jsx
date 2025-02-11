71e23966627607e08de5fafc6d79ae9158183390
ish64 <153824395+2305e3ish@users.noreply.github.com>
2025-02-11 06:21:16 +0000

    adding

--- react/demo/src/App.jsx
+++ react/demo/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//functional component
+//entitlement,roles,tokens
+//jsx - javascript xml
+import "./App.css";
+import {useState} from "react";
+const App = () => {
+  const [val1,setval1] = useState("")
+  const [val2,setval2] = useState("")
+  const [result,setResult] = useState(null)
+  const handleAdd = () => {
+    setResult(parseInt(val1)+parseInt(val2))
+  }
+    return (
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div>
+            <h1>My React App!</h1>
+            <input type="text" placeholder="number1" onChange={(e)=> setval1(e.target.value)}/>
+            <input type="text" placeholder="number2" onChange={(e)=> setval2(e.target.value)}/>
+            <button onClick={handleAdd}>+</button>
+            <p>Result:{result}</p>    
+        </div>
+    );
 }
-
-export default App
+export default App;
