dc633da803212dc9bc63ac2f984df88ebee514cc
ish64 <153824395+2305e3ish@users.noreply.github.com>
2025-03-03 08:53:21 +0000

    multiple pages using react router

--- react/demo/src/App.jsx
+++ react/demo/src/App.jsx
@@ -1,24 +1,19 @@
 //functional component
 //entitlement,roles,tokens
 //jsx - javascript xml
-import "./App.css";
-import {useState} from "react";
-const App = () => {
-  const [val1,setval1] = useState("")
-  const [val2,setval2] = useState("")
-  const [result,setResult] = useState(null)
-  const handleAdd = () => {
-    setResult(parseInt(val1)+parseInt(val2))
-  }
-    return (
+import "react";
+import Home from "./pages/Home";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
 
-        <div>
-            <h1>My React App!</h1>
-            <input type="text" placeholder="number1" onChange={(e)=> setval1(e.target.value)}/>
-            <input type="text" placeholder="number2" onChange={(e)=> setval2(e.target.value)}/>
-            <button onClick={handleAdd}>+</button>
-            <p>Result:{result}</p>    
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Home />
+    <Footer/>
+    </>
+  );
+};
+
 export default App;
